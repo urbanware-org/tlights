1fd54fda262f0036100847a53fd654db2018a711
urbanware-org <github@urbanware.org>
2018-02-01 17:23:34 +0100

    Initial upload

---
+++ README.md
@@ -0,0 +1,52 @@
+# tlights
+
+**Table of contents**
+* [Definition](#definition)
+* [Details](#details)
+* [Requirements](#requirements)
+* [Screenshots](#screenshots)
+
+----
+
+## Definition
+
+This is a simple traffic light control demo which simulates typical traffic light behavior.
+
+[Top](#)
+
+## Details
+
+The project consists of a class to create an traffic light object in combination with a picture box (a *QLabel* used as such to be precise). Additionally there is a timer (default *QTimer*) that automates the switching process.
+
+You can either set the traffic light modes manually or automated.
+
+Manual methods:
+* Set red light only
+* Set yellow light only
+* Set red-yellow combination
+* Set green light
+* Power off
+
+Pre-defined automated methods:
+* Switch the traffic light automatically
+* Set the traffic light to standby
+
+The standby mode simply turns off the traffic light and if it is at a yield position, it will flash with its yellow light.
+
+## Requirements
+
+In order to run the project, the *Python* framework (version 2.6 or higher) must be installed on the system.
+
+The graphical user interface is based on *PyQt4*, which must also be installed.
+
+[Top](#)
+
+## Screenshots
+
+The animated screenshots below are from an earlier version, but are almost identical with the current one.
+
+<img src="gif/day.gif" alt="Day" height="400px" width="400x" align="left"/>
+<img src="gif/night.gif" alt="Night" height="400px" width="400px"/>
+
+[Top](#)
+
---
+++ tlights.py
@@ -0,0 +1,267 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# ============================================================================
+# tlights - Traffic Light Control Demo
+# Copyright (C) 2018 by Ralf Kilian
+#
+# tlights is licensed under GPLv3:
+#
+#     tlights is free software: you can redistribute it and/or modify it
+#     under the terms of the GNU General Public License as published by the
+#     Free Software Foundation, either version 3 of the License, or (at your
+#     option) any later version.
+#
+#     tlights is distributed in the hope that it will be useful, but WITHOUT
+#     ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+#     FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+#     more details.
+#
+#     You should have received a copy of the GNU General Public License along
+#     with tlights. If not, see <http://www.gnu.org/licenses/>.
+#
+# GitHub: https://github.com/urbanware-org/tlights
+# ============================================================================
+
+__version__ = "1.1.0"
+
+import os
+import sys
+from PyQt4 import QtCore, QtGui, uic
+
+interface = os.path.join("ui", "crossing.ui")
+qtCreatorFile = interface
+Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
+
+images = os.path.join("images")
+crossing_day = os.path.join(images, "crossing_day.png")
+crossing_night = os.path.join(images, "crossing_night.png")
+sign_yield = os.path.join(images, "sign_yield.png")
+
+def set_pixmap(obj, pixmap):
+    """
+        Method to simply set a pixmap for a QLabel object.
+    """
+    p = QtGui.QPixmap(pixmap)
+    obj.setPixmap(p)
+
+class TrafficLight(object):
+    """
+        Traffic light control class.
+    """
+    is_red = True
+    is_yield = False
+
+    seconds_red = 3
+    seconds_green = 3
+
+    loop_red = seconds_red
+    loop_green = seconds_green
+
+    standby_signal = 1
+
+    tl_red = os.path.join(images, "trafficlight_red.png")
+    tl_redyellow = os.path.join(images, "trafficlight_redyellow.png")
+    tl_yellow = os.path.join(images, "trafficlight_yellow.png")
+    tl_green = os.path.join(images, "trafficlight_green.png")
+    tl_off = os.path.join(images, "trafficlight_off.png")
+
+    def __init__(self, tl_object, is_red, is_yield, seconds_red,
+                 seconds_green):
+        self.tl_object = tl_object
+        self.set_off()
+
+        self.is_red = is_red
+        self.is_yield = is_yield
+
+        self.seconds_red = seconds_red
+        self.seconds_green = seconds_green
+
+        self.loop_red = seconds_red
+        self.loop_green = seconds_green
+
+    def set_red(self):
+        """
+            Method to only set the red light.
+        """
+        set_pixmap(self.tl_object, self.tl_red)
+
+    def set_redyellow(self):
+        """
+            Method to only set the red and yellow light.
+        """
+        set_pixmap(self.tl_object, self.tl_redyellow)
+
+    def set_yellow(self):
+        """
+            Method to only set the yellow light.
+        """
+        set_pixmap(self.tl_object, self.tl_yellow)
+
+    def set_green(self):
+        """
+            Method to only set the green light.
+        """
+        set_pixmap(self.tl_object, self.tl_green)
+
+    def set_off(self):
+        """
+            Method to disable all lights.
+        """
+        set_pixmap(self.tl_object, self.tl_off)
+
+    def switch(self):
+        """
+            Method to switch the traffic light status by one step.
+        """
+        if self.is_red:
+            if self.loop_red > 0:
+                self.set_red()
+                self.loop_red -= 1
+            else:
+                self.set_redyellow()
+                self.is_red = False
+                self.loop_green = self.seconds_green
+        else:
+            if self.loop_green > 1:
+                self.set_green()
+                self.loop_green -= 1
+            elif self.loop_green == 1:
+                self.set_yellow()
+                self.loop_green = 0
+            else:
+                self.set_red()
+                self.is_red = True
+                self.loop_red = self.seconds_red
+
+    def standby(self):
+        """
+            Method to set standby mode. If the traffic light is at a yield
+            position, it will flash with its yellow light, otherwise it will
+            be completely switched off.
+        """
+        if self.is_yield:
+            if self.standby_signal == 1:
+                self.set_off()
+                self.standby_signal = 0
+            else:
+                self.set_yellow()
+                self.standby_signal = 1
+        else:
+            self.set_off()
+
+    def autoswitch(self, standby_on):
+        """
+            Method to automatically switch the traffic light status step by
+            step, setting the traffic lights on standby at night.
+        """
+        if standby_on:
+            self.standby()
+        else:
+            self.switch()
+
+class TrafficLightsDemo(QtGui.QMainWindow, Ui_MainWindow):
+    """
+        Class for the traffic light control demo.
+    """
+    def __init__(self):
+        QtGui.QMainWindow.__init__(self)
+        Ui_MainWindow.__init__(self)
+        self.setupUi(self)
+
+        self.__set_environment()
+        self.btn_power.clicked.connect(self.__switch_power)
+        self.btn_daylight.clicked.connect(self.__switch_daytime)
+
+    def __set_environment(self):
+        """
+            Start-up method to set the environment.
+        """
+        self.__set_time_day()
+
+        self.tn = TrafficLight(self.tl_north, False, True, 3, 3)
+        self.ts = TrafficLight(self.tl_south, False, True, 3, 3)
+        self.tw = TrafficLight(self.tl_west, True, False, 3, 3)
+        self.te = TrafficLight(self.tl_east, True, False, 3, 3)
+
+        set_pixmap(self.yd_north, sign_yield)
+        set_pixmap(self.yd_south, sign_yield)
+
+        self.__set_time_day()
+        self.__set_power_on()
+
+    def __set_power_on(self):
+        """
+            Switch on traffic lights.
+        """
+        self.btn_power.setText("Standby")
+        self.standby = False
+
+    def __set_power_standby(self):
+        """
+            Switch traffic lights to standby.
+        """
+        self.btn_power.setText("Power on")
+        self.standby = True
+
+    def __switch_power(self):
+        """
+            Switch traffic lights on or to standby.
+        """
+        if self.standby:
+            self.__set_power_on()
+        else:
+            self.__set_power_standby()
+
+    def __set_time_day(self):
+        """
+            Switch to day.
+        """
+        self.btn_daylight.setText("Set night")
+        self.is_day = True
+        set_pixmap(self.bg_crossing, crossing_day)
+
+    def __set_time_night(self):
+        """
+            Switch to night.
+        """
+        self.btn_daylight.setText("Set day")
+        self.is_day = False
+        set_pixmap(self.bg_crossing, crossing_night)
+
+    def __switch_daytime(self):
+        """
+            Switch between day and night.
+        """
+        if self.is_day:
+            self.__set_time_night()
+        else:
+            self.__set_time_day()
+
+    def main_loop(self):
+        """
+            Main loop connected with the timer.
+        """
+        self.tn.autoswitch(self.standby)
+        self.ts.autoswitch(self.standby)
+        self.tw.autoswitch(self.standby)
+        self.te.autoswitch(self.standby)
+
+if __name__ == "__main__":
+    app = QtGui.QApplication(sys.argv)
+    app_icon = QtGui.QIcon()
+    app_icon.addFile(
+        os.path.join(images, "trafficlight_green.png"), QtCore.QSize(48,48))
+    app.setWindowIcon(app_icon)
+
+    tldemo = TrafficLightsDemo()
+    tldemo.show()
+
+    timer = QtCore.QTimer()
+    timer.timeout.connect(tldemo.main_loop)
+    timer.start(1000)
+
+    sys.exit(app.exec_())
+
+# EOF
+
