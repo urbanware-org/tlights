e635c9aac4070e39ebed34b7cc49872408efe301
urbanware-org <github@urbanware.org>
2021-12-29 21:16:40 +0100

    Raise version number

--- tlights.py
+++ tlights.py
@@ -24,7 +24,7 @@
 # GitLab: https://gitlab.com/urbanware-org/tlights
 # ============================================================================
 
-__version__ = "1.1.1"
+__version__ = "1.1.2"
 
 import os
 import sys
